5700a7fa7d902f8ee51d708507b7299ce306bd09
nyang5504 <ninayang3.14@gmail.com>
2022-10-04 20:50:22 -0400

    Edit MixedTournament

--- src/main/scala/MixedTournamentSeason.scala
+++ src/main/scala/MixedTournamentSeason.scala
@@ -1,4 +1,4 @@
-import benjamingarrett.rockpaperscissorstools.{AWins, BWins, RPSHistoryBasedPlayer, RPSOutcome, RPSTournament, RPSTournamentSeason}
+import benjamingarrett.rockpaperscissorstools.{AWins, BWins, RPSHistoryBasedPlayer, RPSOutcome, RPSPointsSchema, RPSTournament, RPSTournamentSeason}
 import IndividualMatchRoundRobinTournament.IndividualMatchRoundRobinTournament
 
 
@@ -10,22 +10,49 @@ class MixedTournamentSeason(val roundsPerMatch: Int) extends RPSTournamentSeason
     }
 
     for(tournament <- tournaments){
+      var SingleTourneyMap = Map.empty[RPSHistoryBasedPlayer,Int]
+      for (player <- players){
+        SingleTourneyMap += player -> 0
+      }
 
-      var player1WinCount = 0
-      var player2WinCount = 0
+
       val SingleTournamentResult = tournament.playTournament(players)
       for((playerPair,resultList) <- SingleTournamentResult){
+        var player1WinCount = 0
+        var player2WinCount = 0
+
         for (result <- resultList){
           if(result == AWins) player1WinCount+=1
           if(result == BWins) player2WinCount+=1
         }
         if(player1WinCount > player2WinCount) {
-          resultMap += playerPair._1 -> (resultMap(playerPair._1)+1)
+          SingleTourneyMap += playerPair._1 -> (SingleTourneyMap(playerPair._1)+1)
         }
         if(player1WinCount < player2WinCount) {
-          resultMap += playerPair._2 -> (resultMap(playerPair._2)+1)
+          SingleTourneyMap += playerPair._2 -> (SingleTourneyMap(playerPair._2)+1)
         }
       }
+      var arrayTourney = SingleTourneyMap.toArray
+      for(i <- 0 to arrayTourney.length-1){
+        for(j <- i to arrayTourney.length-1){
+          if(arrayTourney(j)._2 > arrayTourney(i)._2){
+            val temp = arrayTourney(i)
+            arrayTourney(i) = arrayTourney(j)
+            arrayTourney(j) = temp
+          }
+        }
+      }
+
+      var tourneyRanks = Map.empty[RPSHistoryBasedPlayer,Int]
+      for(i <- 0 to arrayTourney.length-1){
+        tourneyRanks += arrayTourney(i)._1 -> (i+1)
+      }
+      tourneyRanks.map {case (player, points) => player -> RPSPointsSchema.pointsAwarded(points)}
+
+      for((key, value) <- tourneyRanks){
+        resultMap += key -> (value + resultMap(key))
+      }
+
     }
     resultMap
   }
